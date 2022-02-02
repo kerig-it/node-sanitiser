fa8cfcfcfc7a48bccf9f29b221d3998e42d16455
msfninja <msfninja@pm.me>
2022-02-02 18:50:56 +0100

    Update index.js
     - Add `options` parameter to sanitise() function.
     - Add checkOptions() function for validating supplied options.

--- index.js
+++ index.js
@@ -1,102 +1,127 @@
-/*
-	A path name sanitiser.
-
-	You can find documentation on the usage of this package in the
-	README file in this repository.
-
-	Github: https://github.com/kerig-it/sanitiser
-*/
+// Sanitiser
+//
+// sanitiser is a path name sanitiser.
+//
+// You can find documentation on the usage of this package in the
+// README file in this repository.
+//
+// GitHub: https://github.com/kerig-it/sanitiser
+// npm: https://www.npmjs.com/package/sanitiser
 
 
 // Node modules (these, among others, are added as dependancies in the
 // `package.json` file, so you'd only need to run `npm i` in the
 // terminal to install all dependancies).
-
 const truncate = require('truncate-utf8-bytes');
 
+// Define allowed options.
+let allowedOptions = [
+	'ignoreControl',
+	'ignoreIllegal',
+	'ignoreRelative'
+];
 
-// **The Sanitiser**
+// Options validator
+const checkOptions = object => {
 
-const sanitise = (pathname, replacement) => {
+	// Is `object` an object?
+	if (typeof(objext) === 'object') {
+		
+		// Loop over all object properties.
+		for (const property in object) {
+
+			// Is the property not one of the allowed options?
+			if (!allowedOptions.includes(property)) {
+				// Throw an error.
+				throw new Error(`The ${property} property is invalid.`);
+			}
+		}
+
+		// Return a successful status.
+		return true;
+	}
+
+	// Throw an error.
+	throw new Error('The \`options\` argument must of type object.');
+};
+
+
+// Sanitiser
+const sanitise = (pathname, replacement, options) => {
+
+	// Are any options supplied?
+	if (options) {
+		// Validate the supplied options.
+		checkOptions(options);
+	}
 
 	// Is `pathname` not a string?
-
 	if (typeof(pathname) !== 'string') {
 
 		// String conversion
-
 		try {
 			// Try to convert it to a string.
 			pathname = pathname.toString();
 		}
 		catch (error) {
-
 			// If there was an error in converting the supplied
 			// `pathname` argument to a string, throw an error.
-
 			throw new Error('Passed argument must be of type string.');
 		}
 	}
 
-
 	// Truncate `pathname` string to 4096 bytes.
-
 	pathname = truncate(pathname, 4096);
 
-
 	// Define regular expressions for validation.
-
 	let
-		// Illegal characters
-		illegal = /[\?<>:\*\|"]/g,
-
 		// Control characters
 		control = /[\x00-\x1f\x80-\x9f]/g,
+
+		// Illegal characters
+		illegal = /[\?<>:\*\|"]/g,
 
 		// Relative paths
 		relative = /^\.+$/;
 
-
 	// Was a replacer not set?
-
 	if (!replacement) {
 		// Set the replacer to empty an string.
 		replacement = '';
 	}
 
-
 	// Define the `sanitised` variable. This will eventually be
 	// returned by this function and will hold the sanitised path name.
-
 	let sanitised = '';
 
-
 	// Loop over all items of the supplied path name.
-
 	pathname.split('/').forEach(e => {
 
 		// Validate the item with previously defined regular
 		// expressions and append it to the `sanitised` variable.
 
-		sanitised += '/' + e // <-- Current directory/file in the path.
-
-			// Replace illegal characters.
-			.replace(illegal, replacement)
-
+		// Is `ignoreControl` disabled?
+		if (!options.ignoreControl) {
 			// Replace control characters.
-			.replace(control, replacement)
+			sanitised += '/' + e.replace(control, replacement);
+		}
 
+		// Is `ignoreIllegal` disabled?
+		if (!options.ignoreIllegal) {
+			// Replace illegal characters.
+			sanitised += '/' + e.replace(illegal, replacement);
+		}
+
+		// Is `ignoreRelative` disabled?
+		if (!options.ignoreRelative) {
 			// Replace attempts to make a relative path.
-			.replace(relative, replacement);
+			sanitised += '/' + e.replace(relative, replacement);
+		}
 	});
 
-
 	// Return the modified string of the pathname.
-
 	return sanitised;
 };
 
-
 // Export the `sanitise` function.
-
 module.exports = sanitise;
