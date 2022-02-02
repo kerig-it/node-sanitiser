2d2bd43386cfade47f706d1a55c0278bdeed509b
msfninja <msfninja@pm.me>
2022-02-02 14:12:11 +0100

    Update index.js
    Reduce the amount of characters per line to 70 or less, if possible, improving overall readability.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 /*
 	A path name sanitiser.
 
-	Github:
-	https://github.com/kerig-it/sanitise
+	You can find documentation on the usage of this package in the
+	README file in this repository.
+
+	Github: https://github.com/kerig-it/sanitiser
 */
 
 
-// Modules
+// Node modules (these, among others, are added as dependancies in the
+// `package.json` file, so you'd only need to run `npm i` in the
+// terminal to install all dependancies).
 
 const truncate = require('truncate-utf8-bytes');
 
 
-// Sanitizer
+// **The Sanitiser**
 
 const sanitise = (pathname, replacement) => {
 
@@ -22,14 +26,13 @@ const sanitise = (pathname, replacement) => {
 		// String conversion
 
 		try {
-
 			// Try to convert it to a string.
-
 			pathname = pathname.toString();
 		}
 		catch (error) {
 
-			// Throw error.
+			// If there was an error in converting the supplied
+			// `pathname` argument to a string, throw an error.
 
 			throw new Error('Passed argument must be of type string.');
 		}
@@ -41,63 +44,49 @@ const sanitise = (pathname, replacement) => {
 	pathname = truncate(pathname, 4096);
 
 
-	// Define regular expressions.
+	// Define regular expressions for validation.
 
 	let
-
 		// Illegal characters
-
 		illegal = /[\?<>\\:\*\|"]/g,
 
-
 		// Control characters
-
 		control = /[\x00-\x1f\x80-\x9f]/g,
 
-
 		// Relative paths
-
 		relative = /^\.+$/;
 
 
 	// Was a replacer not set?
 
 	if (!replacement) {
-
-		// Set to empty string.
-
+		// Set the replacer to empty an string.
 		replacement = '';
 	}
 
 
-	// Define `sanitised` variable.
+	// Define the `sanitised` variable. This will eventually be
+	// returned by this function and will hold the sanitised path name.
 
 	let sanitised = '';
 
 
-	// Loop over all pathname items.
+	// Loop over all items of the supplied path name.
 
 	pathname.split('/').forEach(e => {
 
-		/*
-			Validate the item with previously defined regular expressions and append
-			it to the `sanitised` variable.
-		*/
+		// Validate the item with previously defined regular
+		// expressions and append it to the `sanitised` variable.
 
-		sanitised += '/' + e
+		sanitised += '/' + e // <-- Current directory/file in the path.
 
 			// Replace illegal characters.
-
 			.replace(illegal, replacement)
 
-
-			// Replace control characters. (temporarily disabled)
-
+			// Replace control characters.
 			.replace(control, replacement)
 
-
 			// Replace attempts to make a relative path.
-
 			.replace(relative, replacement);
 	});
 
@@ -108,6 +97,6 @@ const sanitise = (pathname, replacement) => {
 };
 
 
-// Export
+// Export the `sanitise` function.
 
 module.exports = sanitise;
