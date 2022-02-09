e00b9291a386409fbc448352e89f7bbf9a7df219
msfninja <msfninja@pm.me>
2022-02-09 15:58:36 +0100

    Update `test.js`
    Add testing.

--- test/test.js
+++ test/test.js
@@ -1,1 +1,17 @@
-console.log('test');
+// Tester of `sanitiser`.
+
+// Node modules
+const
+	fs = require('fs'),
+	sanitiser = require('sanitiser');
+
+// Read `paths.list`.
+let paths = fs.readFile('paths.list',(error, data) => {
+
+	// Loop over all entries.
+	data.toString().split('\n').forEach(e => {
+		// Log sanitisation.
+		console.log(`Path     : ${e}`);
+		console.log(`Sanitised: ${sanitiser(e)}\n`);
+	});
+});
