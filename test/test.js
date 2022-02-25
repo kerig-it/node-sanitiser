a610d7fff61c711168023919213b8762ca7fc5ea
msfninja <msfninja@pm.me>
2022-02-25 10:47:06 +0100

    Update `test.js`
    Add more elaboration and testing.

--- test/test.js
+++ test/test.js
@@ -1,17 +1,33 @@
-// Tester of `sanitiser`.
+/*
+ * Test script for the `sanitiser` package.
+ *
+ * Refer to the README in the root directory of this repository for
+ * more information on testing this package.
+ *
+ * Website: https://pkg.kerig.ee/sanitiser
+ * GitHub:  https://github.com/kerig-it/sanitiser
+ * npm:     https://www.npmjs.com/package/sanitiser
+ *
+ * Made with ❤️ by Kerig.
+*/
 
 // Node modules
 const
 	fs = require('fs'),
 	sanitiser = require('sanitiser');
 
-// Read `paths.list`.
-let paths = fs.readFile('paths.list',(error, data) => {
+// Read the `paths` file.
+let paths = fs.readFile('paths', (error, data) => {
 
-	// Loop over all entries.
-	data.toString().split('\n').forEach(e => {
-		// Log sanitisation.
-		console.log(`Path     : ${e}`);
-		console.log(`Sanitised: ${sanitiser(e)}\n`);
+	// Error handling
+	if (error) {
+		throw error;
+	}
+
+	// Loop over all path names.
+	data.toString().split('\n').forEach(pathname => {
+		// Log "before" and "after" the sanitisation of the path name.
+		console.log(`Supplied:  ${pathname}`);
+		console.log(`Sanitised: ${sanitiser(pathname)}\n`);
 	});
 });
